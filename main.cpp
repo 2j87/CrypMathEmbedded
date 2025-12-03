901a5ec5e0d92634f3a87548f5c00846071a9593
superroket169 <isagullu2010@gmail.com>
2025-12-03 16:31:06 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <algorithm>
+#include <cmath>
+
+int charToInt(char ch)
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7,8, 9,10,11, 20, 22, 24, 26, 28, 30, 32, 34, 35, 59, 62, 65, 68, 71, 74, 77,80,81,127,131,
+135,139,143,147,151,155,156, 231, 236, 241, 246, 251, 256, 261, 266, 267, 378, 384, 390, 396, 402,
+408, 414, 420, 421, 575, 582, 589, 596, 603, 610, 617, 624, 625,829,837,845,853,861,869,877,885,
+886,1165,1174,1183,1192,1201,1210,1211,1536,1546,1556,1566,1576,1586,1606,1607, 2014};
+
+    int retrn = -1;
+    switch(ch)
+    {
+        case 'A': retrn = arr[0]; break;
+        case 'B': retrn = arr[1]; break;
+        case 'C': retrn = arr[2]; break;
+        case 'D': retrn = arr[3]; break;
+        case 'E': retrn = arr[4]; break;
+        case 'F': retrn = arr[5]; break;
+        case 'G': retrn = arr[6]; break;
+        case 'H': retrn = arr[7]; break;
+        case 'I': retrn = arr[8]; break;
+        case 'J': retrn = arr[9]; break;
+        case 'K': retrn = arr[10]; break;
+        case 'L': retrn = arr[11]; break;
+        case 'M': retrn = arr[12]; break;
+        case 'N': retrn = arr[13]; break;
+        case 'O': retrn = arr[14]; break;
+        case 'P': retrn = arr[15]; break;
+        case 'Q': retrn = arr[16]; break;
+        case 'R': retrn = arr[17]; break;
+        case 'S': retrn = arr[18]; break;
+        case 'T': retrn = arr[19]; break;
+        case 'U': retrn = arr[20]; break;
+        case 'V': retrn = arr[21]; break;
+        case 'W': retrn = arr[22]; break;
+        case 'X': retrn = arr[23]; break;
+        case 'Y': retrn = arr[24]; break;
+        case 'Z': retrn = arr[25]; break;
+        case 'a': retrn = arr[26]; break;
+        case 'b': retrn = arr[27]; break;
+        case 'c': retrn = arr[28]; break;
+        case 'd': retrn = arr[29]; break;
+        case 'e': retrn = arr[30]; break;
+        case 'f': retrn = arr[31]; break;
+        case 'g': retrn = arr[32]; break;
+        case 'h': retrn = arr[33]; break;
+        case 'i': retrn = arr[34]; break;
+        case 'j': retrn = arr[35]; break;
+        case 'k': retrn = arr[36]; break;
+        case 'l': retrn = arr[37]; break;
+        case 'm': retrn = arr[38]; break;
+        case 'n': retrn = arr[39]; break;
+        case 'o': retrn = arr[40]; break;
+        case 'p': retrn = arr[41]; break;
+        case 'q': retrn = arr[42]; break;
+        case 'r': retrn = arr[43]; break;
+        case 's': retrn = arr[44]; break;
+        case 't': retrn = arr[45]; break;
+        case 'u': retrn = arr[46]; break;
+        case 'v': retrn = arr[47]; break;
+        case 'w': retrn = arr[48]; break;
+        case 'x': retrn = arr[49]; break;
+        case 'y': retrn = arr[50]; break;
+        case 'z': retrn = arr[51]; break;
+        case '.': retrn = arr[52]; break;
+        case '(': retrn = arr[53]; break;
+        case ')': retrn = arr[54]; break;
+        case ',': retrn = arr[55]; break;
+        case ';': retrn = arr[56]; break;
+        case ':': retrn = arr[57]; break;
+        case '\'': retrn = arr[58]; break;
+        case '@': retrn = arr[59]; break;
+        case '"': retrn = arr[60]; break;
+        case '?': retrn = arr[61]; break;
+        case '!': retrn = arr[62]; break;
+        case '/': retrn = arr[63]; break;
+        case '-': retrn = arr[64]; break;
+        case '+': retrn = arr[65]; break;
+        case '=': retrn = arr[66]; break;
+        case ' ': retrn = arr[67]; break;
+        case '0': retrn = arr[68]; break;
+        case '1': retrn = arr[69]; break;
+        case '2': retrn = arr[70]; break;
+        case '3': retrn = arr[71]; break;
+        case '4': retrn = arr[72]; break;
+        case '5': retrn = arr[73]; break;
+        case '6': retrn = arr[74]; break;
+        case '7': retrn = arr[75]; break;
+        case '8': retrn = arr[76]; break;
+        case '9': retrn = arr[77]; break;
+    }
+
+    return retrn;
+}
+
+struct CordinateEcef
+{
+    //meter type
+    int x;
+    int y;
+    int z;
+};
+
+struct CordinateGps
+{
+    int GpsLat;//Enlem x1 000 000 degree
+    int GpsLon;//Boylam x1 000 000 degree
+};
+
+int DisDU(CordinateEcef s, CordinateEcef r)//Disdyakis Dodecahedron Uzaklık
+{
+    int absX = std::abs(s.x - s.x);
+    int absY = std::abs(s.y - s.y);
+    int absZ = std::abs(s.z - s.z);
+
+    int el1 = std::max(absX, absY, absZ);
+    int el2 = (std::sqrt(2) - 1) * std::min(absX + absY , absY + absZ, absZ + absX);
+    int el3 = (std::sqrt(2) - 2 * std::sqrt(2) + 1 ) * std::min(absX, absY, absZ);
+
+    return el1 + el2 + el3;
+}
+
+int TetHU(CordinateEcef s, CordinateEcef r)//Tetrakis Hexahedron Uzaklık
+{
+    int absX = std::abs(s.x - s.x);
+    int absY = std::abs(s.y - s.y);
+    int absZ = std::abs(s.z - s.z);
+
+    int el1 = (std::sqrt(3) - 1) * std::max(absX + absY, absX + absZ, absY + absY);
+    int el2 = (std::sqrt(3) - 1) * std::max(absX, absY, absZ);
+
+    return el1 + el2;
+}
+
+int TruOU(CordinateEcef s, CordinateEcef r)//Truncated Octahedron Uzaklık
+{
+    int absX = std::abs(s.x - s.x);
+    int absY = std::abs(s.y - s.y);
+    int absZ = std::abs(s.z - s.z);
+
+    return std::max((2/3*(absX + absY + absZ)), std::max(absX, absY, absZ));
+}
+
+int TriOU(CordinateEcef s, CordinateEcef r)//Triakis Octahedron Uzaklık
+{
+    int absX = std::abs(s.x - s.x);
+    int absY = std::abs(s.y - s.y);
+    int absZ = std::abs(s.z - s.z);
+
+    int el1 = (std::sqrt(2) - 2) * std::min(absX, absY, absZ);
+
+    return absX + absY + absZ + el1;
+}
+
+signed main()
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::tm* time = std::localtime(&t);
+    //time->tm_hour // saat
+    //time->tm_min // dakika
+    //std::cout << time->tm_hour << "\n";
+
+    std::string input;
+    std::cin >> input;
+
+    std::vector<int> vecint(input.size());
+    for(int i = 0 ; i < vecint.size() ; ++i) vecint[i] = charToInt(input[i]);
+    
+    //GPS to ECEF
+    CordinateEcef SenderCord = {4113913, 3440529, 3440829};
+    CordinateEcef ReceiverCord = {1118567, 902131, -6193309};
+    //
+
+
+
+}
